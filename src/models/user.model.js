48c1f97887f17fe841d61bc720aa770426b91732
Vansh Baranwal <vanshveerbaranwal@gmail.com>
2026-08-28 02:54:14 +0530

    fixed a typo

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -30,13 +30,13 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function(next) {
 
     if(!this.isModified("password")){
-        return next();
+        return next;
     };
 
     const hash = await bcrypt.hash(this.password, 10);
     this.password = hash;
 
-    return next();
+    return next;
 });
 
 userSchema.methods.comparePassword = async function(password){
