4b56fe31fe41fced724693bc2333091e4ef8a96d
Vansh Baranwal <vanshveerbaranwal@gmail.com>
2026-08-31 19:14:05 +0530

    add user accounts and balance retrieval

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -8,7 +8,8 @@ const router = express.Router();
 
 router.post("/", authMiddleware.authMiddleware, accountController.createAccountController);
 
+router.get("/", authMiddleware.authMiddleware, accountController.getUserAccountsController);
 
-
+router.get("/balance/:accountId", authMiddleware.authMiddleware, accountController.getAccountBalanceController);
 
 module.exports = router;
